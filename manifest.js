7c7b671fef003daeba5a8e6267739c217d12fa3c
Ron @ DDB Chicago <ronlagon_ddb@yahoo.com>
2016-06-06 12:02:21 -0500

    v2.0.1 - Aether
    Added additional Comments / Info on the role of manifest.js

--- manifest.js
+++ manifest.js
@@ -2,22 +2,26 @@ FT.manifest({
 	"filename":"index.html",
 	"width":300,
 	"height":250,
+	
+	//	The clickTagCount includes the default clicktag unlike Platform.JS, which just needs any custom elements that have additional clickthrus
 	"clickTagCount":1,
+	
+	//	ONLY needed if the unit is to be set up as a dynamic (Instant) Ad Unit
 	"instantAds":[
+		// Each JSON entry must have the following format for each of the dynamic values
 		{
-			"name":"end_tagline", 
+			//"name" : "Name of Value (Same Name listed in Platform.JS option - $dynVars", 
+			//"type" : "{text or image}", 
+			//"default" : "The Default value that will be present if none fed to by FT."
+			
+			"name":"some_text_variable", 
 			"type":"text", 
-			"default":"Unexplainably Juicy."
+			"default":"Some Default Copy Here."
 		},
 		{
-			"name":"fb_copy", 
-			"type":"text", 
-			"default":"share your #juicytheory"
-		},
-		{
-			"name":"dance_img", 
+			"name":"some_img_variable", 
 			"type":"image", 
-			"default":"images/Crab_Walk-2.gif"
+			"default":"some_def_img.jpg"
 		}
 	]
 });
