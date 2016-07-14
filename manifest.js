e9fb2e745872e1b0b87561eaf06f271815ebc509
Ron @ DDB Chicago <ronlagon_ddb@yahoo.com>
2016-07-14 10:15:33 -0500

    v2.4.0 - Ares
    Additional File added - Comments are validated as errors

--- manifest.js
+++ manifest.js
@@ -1,30 +1,18 @@
-/*	version: "v2.4.0 - Ares"	*/
-
-
 FT.manifest({
 	"filename":"index.html",
 	"width":300,
 	"height":250,
-	
-	//	The clickTagCount includes the default clicktag unlike Platform.JS, which just needs any custom elements that have additional clickthrus
 	"clickTagCount":1,
-	
-	//	ONLY needed if the unit is to be set up as a dynamic (Instant) Ad Unit
 	"instantAds":[
-		// Each JSON entry must have the following format for each of the dynamic values
 		{
-			//"name" : "Name of Value (Same Name listed in Platform.JS option - $dynVars", 
-			//"type" : "{text or image}", 
-			//"default" : "The Default value that will be present if none fed to by FT."
-			
-			"name":"some_text_variable", 
-			"type":"text", 
-			"default":"Some Default Copy Here."
+			"name":"frame_1_bg", 
+			"type":"image", 
+			"default":"images/SS_Pot_Pie/frame_1_bg.jpg"
 		},
 		{
-			"name":"some_img_variable", 
-			"type":"image", 
-			"default":"some_def_img.jpg"
+			"name":"copy1_line1", 
+			"type":"text", 
+			"default":"Homestyle recipe."
 		}
 	]
 });
