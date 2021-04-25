8758b69e1f7f523752814256b605402e684e1a32
Ronald LaGon <ronald.lagon@ddb.com>
2021-04-25 17:31:49 -0500

    Addition of the Sizmek config file to include fof unit upload into the API

---
+++ config.js
@@ -0,0 +1,26 @@
+/*
+ Manifest Setup for Sizmek Standard Banner Ad -->
+*/
+
+define({
+	"adFormatId": "42",
+	"format": "standardBanner",
+	"name": "",
+	"defaultBanner": "Main_Banner",
+	"banners": [{
+		"name": "Main_Banner",
+		"width": 320,
+		"height": 250,
+		"defaultImage": "images/320x250_DFLT.jpg"
+	}],
+	"clickThrough": {
+		"url": "https://www.google.com/",
+		"target": "newWindow",
+		"showMenuBar": true,
+		"showAddressBar": true
+	}
+});
+
+/***********************************************************************************************************************************************************************/
+/* More Documantation on the Sizmek API located here: https://support.sizmek.com/hc/en-us/articles/360055240931-Manifest-Ad-Data#ManifestDataParameters */
+/***********************************************************************************************************************************************************************/
