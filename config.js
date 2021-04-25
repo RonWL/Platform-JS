8826566b30bf19d00416db2e82351c63d3e71f59
Ronald LaGon <ronald.lagon@ddb.com>
2021-04-25 17:42:24 -0500

    Comments entered to explain a few of the config.js parameters

--- config.js
+++ config.js
@@ -5,10 +5,10 @@
 define({
 	"adFormatId": "42",
 	"format": "standardBanner",
-	"name": "",
-	"defaultBanner": "Main_Banner",
+	"name": "", /* Name of the creative - usually the Folder housing all assets of the unit - Keep away from spaces as this will throw errors */
+	"defaultBanner": "Main_Banner", /* The default asset shown - usually the Folder housing all assets of the unit - Keep away from spaces as this will throw errors */
 	"banners": [{
-		"name": "Main_Banner",
+		"name": "Main_Banner", /* Name of the unit - usually the Folder housing all assets of the unit - Keep away from spaces as this will throw errors */
 		"width": 320,
 		"height": 250,
 		"defaultImage": "images/320x250_DFLT.jpg"
